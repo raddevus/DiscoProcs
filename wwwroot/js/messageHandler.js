f89dc0757332ce2f81dd723f3f36ca0f9b56e226
raddevus <1radeutsch1@gmail.com>
2024-12-07 18:36:17 -0500

    had to add a lot of code to parse off the proc name from the file name for times when there is no proc name.  That code may not work on Linux or Windows -- need to test.

--- wwwroot/js/messageHandler.js
+++ wwwroot/js/messageHandler.js
@@ -104,6 +104,19 @@ var allCommands = ["getCurrentDirectory",
     let message = {};
     message.Command = "getProcInfoByName";
     var procName = document.querySelector("#procList").selectedOptions[0].textContent;
+    // if it's equal to empty string, then we have to use the name from the details
+    // bec this is an un-named proc
+    // ## NOTE: Convert procName to an integer value -- bec if it is named after
+    // the procId then it doens't have a procname and we have to get it from the filename
+    if (procName >= 0){
+      var fileText = document.querySelector("#pfile").textContent;
+      var startIdx = fileText.lastIndexOf("/") >= 0 ? fileText.lastIndexOf("/") : fileText.lastIndexOf("\\");
+      //increment startIdx by one bec need to skip / or \ symbol
+      ++startIdx;
+      if (startIdx >=0){
+        procName = fileText.substring(startIdx);
+      }
+    }
     message.Parameters = procName;
     let sMessage = JSON.stringify(message);
     sendMessage(sMessage);
