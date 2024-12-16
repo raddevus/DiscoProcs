1cf571d35ad1b52f877c8486a4e95dc1c5445032
raddevus <1radeutsch1@gmail.com>
2024-12-16 16:30:54 -0500

    renaming params so they are more generic

--- wwwroot/js/resultTable.js
+++ wwwroot/js/resultTable.js
@@ -1,32 +1,32 @@
-function displayResultTable(procEntries, rootElement, targetFunc){
-    console.log(`userTasks ${JSON.stringify(procEntries)}`);
+function displayResultTable(dataRows, rootElement, targetFunc){
+    console.log(`userTasks ${JSON.stringify(dataRows)}`);
     //initUserTaskTable();
     ReactDOM.render(
         // We are passing in just the tasks - not the outer object 
         // which includes the success property
-        targetFunc (procEntries),
+        targetFunc (dataRows),
         document.querySelector(rootElement),
         hideWaitCursor("#history-spinner")
     );
 }
 
-const buildResultTable = function(procEntries){
+const buildResultTable = function(dataRows){
 
-    console.log(procEntries[0]);
+    console.log(dataRows[0]);
     
      let allItems = [];
 
-    for (let x=0; x < procEntries.length;x++){
-        console.log(`procEntries[${x}].Name: ${procEntries[x].Name}`);
-        let currentId = procEntries[x].id;
-        allItems.push( React.createElement("tr",{key:x, id:procEntries[x].id},
+    for (let x=0; x < dataRows.length;x++){
+        console.log(`procEntries[${x}].Name: ${dataRows[x].Name}`);
+        let currentId = dataRows[x].id;
+        allItems.push( React.createElement("tr",{key:x, id:dataRows[x].id},
         
-        React.createElement("td",{id:`name-${currentId}`}, procEntries[x].Name),
-        React.createElement("td",{id:`filename-${currentId}`}, procEntries[x].Filename),
-        React.createElement("td",{id:`filedate-${currentId}`}, procEntries[x].FileDate),
-        React.createElement("td",{id:`filehash-${currentId}`}, procEntries[x].FileHash),
-        React.createElement("td",{id:`filesize-${currentId}`}, procEntries[x].FileSize),
-        React.createElement("td",{id:`created-${currentId}`,width:"150px"},  procEntries[x].Created),
+        React.createElement("td",{id:`name-${currentId}`}, dataRows[x].Name),
+        React.createElement("td",{id:`filename-${currentId}`}, dataRows[x].Filename),
+        React.createElement("td",{id:`filedate-${currentId}`}, dataRows[x].FileDate),
+        React.createElement("td",{id:`filehash-${currentId}`}, dataRows[x].FileHash),
+        React.createElement("td",{id:`filesize-${currentId}`}, dataRows[x].FileSize),
+        React.createElement("td",{id:`created-${currentId}`,width:"150px"},  dataRows[x].Created),
                 
             )
        );
