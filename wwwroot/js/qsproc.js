971ba20b7f91cf1fde5941549161bfbfc6534e05
raddevus <1radeutsch1@gmail.com>
2024-12-25 17:55:44 -0500

    Work to add the quick start process js class

---
+++ wwwroot/js/qsproc.js
@@ -0,0 +1,6 @@
+// qsproc is Quick Start Process class
+// This class helps wrap up the properties
+// for the procesess the user will  use to 
+// add one or more sets of processes she 
+// wants to start from DiscProcs
+
